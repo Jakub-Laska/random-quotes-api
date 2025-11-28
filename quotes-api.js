b5c566efd1de5185f6a845ced51061cf9b385edf
Jakub-Laska <laska.rayl@gmail.com>
2025-11-28 15:49:03 +0100

    first commit

---
+++ quotes-api.js
@@ -0,0 +1,14 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+
+    res.status(200).json({
+      quote: data.content,
+      author: data.author,
+      source: "quotable.io"
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Nie udało się pobrać cytatu" });
+  }
+}
